d22b868bede03b7ff52c94414a30ca2069a8452c
alexlao <lao.alex97@gmail.com>
2015-10-28 19:19:43 -0700

    Fixed counter again

--- Milestone2Demo/Counter.java
+++ Milestone2Demo/Counter.java
@@ -21,7 +21,7 @@ public class Counter extends Actor
     }
     public void bumpCounter(){
         totalCount++;
-        setImage(new GreenfootImage(""+totalCount, 20, Color.BLACK, transparent));
+        setImage(new GreenfootImage("Score: "+totalCount, 20, Color.BLACK, transparent));
     }
     public int returnValue(){
         //returns the number on the counter
@@ -31,7 +31,7 @@ public class Counter extends Actor
     {
         //changes the counter value when item is purchased
         totalCount = totalCount - x;
-        setImage(new GreenfootImage(""+totalCount, 20, Color.BLACK, transparent));
+        setImage(new GreenfootImage("Score: "+totalCount, 20, Color.BLACK, transparent));
     }
     public void gainPoints(int z){
         totalCount = totalCount + z;
