cb5fabcf6ef8aec697d732b0dce9d03ea67d7a90
alexlao <lao.alex97@gmail.com>
2015-11-07 02:46:34 -0800

    Even more updated first world tutorial.

---
+++ DreamWIP/EndBubble.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class EndBubble here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EndBubble extends ScrollingActor
+{
+    /**
+     * Act - do whatever the EndBubble wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
