7a464648bf4dab79af5bf123ae3b9f43748ef2f9
zargut <zarahgutierrez@gmail.com>
2015-11-12 18:30:14 -0800

    working menu (only start button for now)

---
+++ DreamWIP/HaungsMode.java
@@ -0,0 +1,49 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class HaungsMode here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HaungsMode extends StartMenuButtons
+{
+    int time = 0;
+    /**
+     * Act - do whatever the HaungsMode wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(Greenfoot.mouseClicked(this))
+        {
+           time = 24;
+        }
+        if (time == 21)
+        {
+           
+        }
+        if (time == 18)
+        {
+        }
+        if (time == 15)
+        {
+        }
+        if (time == 12)
+        {
+                        
+        }
+        if (time == 9)
+        {
+                      
+        }
+        if (time == 6)
+        {
+
+        }
+        if (time == 3)
+        {
+             
+        } // Add your action code here.
+    }    
+}
--- DreamWIP/StartMenu.java
+++ DreamWIP/StartMenu.java
@@ -12,17 +12,38 @@ public class StartMenu extends World
     public StartMenu()
     {
         super (1000,600, 1);
-    }
+
+        prepare();
+    }
+
     /**
      * Constructor for objects of class StartMenu.
      * 
      */
     public void act()
     {    
-                   setBackground(Image.getCurrentImage());
-                   if(Greenfoot.mouseClicked(this))
-                   {
-                       Greenfoot.setWorld(new TutorialWorld());
-                   }
+        setBackground(Image.getCurrentImage());
+       
+    }
+
+    /**
+     * Prepare the world for the start of the program. That is: create the initial
+     * objects and add them to the world.
+     */
+    private void prepare()
+    {
+        HaungsMode haungsmode = new HaungsMode();
+        addObject(haungsmode, 734, 562);
+        haungsmode.setLocation(734, 552);
+        haungsmode.setLocation(737, 469);
+        Credits credits = new Credits();
+        addObject(credits, 798, 557);
+        credits.setLocation(795, 552);
+        Start start = new Start();
+        addObject(start, 178, 473);
+        start.setLocation(178, 472);
+        start.setLocation(178, 471);
+        start.setLocation(161, 467);
+        start.setLocation(161, 469);
     }
 }
