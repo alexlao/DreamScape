8d2a0382a0d0d201f01ceb63f60cad963fd93ff7
zargut <zarahgutierrez@gmail.com>
2015-11-17 09:01:39 -0800

    new start menu icons

---
+++ DreamWIP/HighScores.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class HighScores here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HighScores extends StartMenuButtons
+{
+    /**
+     * Act - do whatever the HighScores wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
