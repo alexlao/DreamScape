ee7a94a4401e5508a125efdb5827a1fd597a1292
alexlao <lao.alex97@gmail.com>
2015-12-06 16:01:07 -0800

    Fixed image error.

--- DreamWIP/GameOver.java
+++ DreamWIP/GameOver.java
@@ -16,7 +16,7 @@ public class GameOver extends World
     public GameOver()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000, 600, 1); 
+        super(1000, 600, 1);
         //a = player;
         //x = td1.purchasedAmount();
     }
